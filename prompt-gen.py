4aee004516881730ba4298a48387bfa8ef30b3fb
Adam Nobody <uliya.ru3456@gmail.com>
2023-01-30 16:41:23 +0300

    Add some tags

--- prompt-gen.py
+++ prompt-gen.py
@@ -4,9 +4,11 @@ girlsNumber = ["1girl", "2girls", "3girls"]
 
 censorship = ["censored", "nude"]
 
-pose = ["full_growth", "bust", "portrait"]
+typeP = ["full_growth", "bust", "portrait"]
 
-skinColor = ["white", "black"]
+pose = ["victory_pose", "bunny_pose", "69 pose"]
+
+skinColor = ["light", "black"]
 
 outfit = ["dress", "swimsuit", "school_uniform", "lingerie", "suit", "t-shirt", "skirt"]
 outfitColor = ["white", "black", "red", "gray", "green", "yellow", "purple"]
@@ -15,14 +17,14 @@ accessories = ["hat", "umbrella", "no_accessories", "smartphone", "watch", "chok
 
 jewelry = ["no_jewelry", "necklace", "medallion", "gems", "bracelet"]
 
-hairLong = ["cut", "long", "very long"]
+hairLong = ["cut", "long", "very_long"]
 hairColor = ["blonde", "black", "green", "red", "blue", "brown", "strawberry_blonde", "golden_blonde"]
 
-eyesColor = ["blue", "green", "brown", "dark", "red", "amber", "gray"]
+eyesColor = ["blue", "green", "brown", "yellow", "dark", "red", "amber", "gray"]
 
 breasts = ["small", "medium", "big", "huge", "very_huge"]
 
-background = ["white_background", "swimming_pool", "forest", "beach", "classroom", "sky", "street"]
+background = ["white_background", "swimming_pool", "forest", "bedroom", "beach", "classroom", "sky", "street"]
 
-prompt = f"{random.choice(girlsNumber)},{random.choice(censorship)}, {random.choice(pose)}, {random.choice(skinColor)}_skin, pussy, {random.choice(outfitColor)}_{random.choice(outfit)}, {random.choice(accessories)}, {random.choice(jewelry)},{random.choice(hairColor)}_{random.choice(hairLong)}_hair, {random.choice(eyesColor)}_eyes, {random.choice(breasts)}_breasts, nipples, {random.choice(background)}"
+prompt = f"{random.choice(girlsNumber)},{random.choice(censorship)}, {random.choice(typeP)}, {random.choice(pose)}, {random.choice(skinColor)}_skin, pussy, {random.choice(outfitColor)}_{random.choice(outfit)}, {random.choice(accessories)}, {random.choice(jewelry)},{random.choice(hairColor)}_{random.choice(hairLong)}_hair, {random.choice(eyesColor)}_eyes, {random.choice(breasts)}_breasts, nipples, {random.choice(background)}"
 print(prompt)
